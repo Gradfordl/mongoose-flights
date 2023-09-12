99067fd07060190bfdbd116c0a570f0f49798ac1
gradfordl <laurengradford@gmail.com>
2023-09-12 18:02:06 -0400

    corrected default date code

--- models/Flight.js
+++ models/Flight.js
@@ -1,5 +1,13 @@
 const { Schema, model } = require("mongoose");
 
+const destinationSchema = new Schema({
+    airport: {
+        type: String,
+        enum: ['AUS', 'DAL', 'LAX', 'SAN', 'SEA']
+      },
+    arrival: Date
+});
+
 const flightSchema = new Schema({
   airline: {
     type: String,
@@ -14,8 +22,14 @@ const flightSchema = new Schema({
     type: Date,
     date: {type: Date, default: Date.now() + 3652460*60000}
     },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DAL', 'LAX', 'SAN', 'SEA']
+      },
+      destinations: [destinationSchema]
   },
 );
+
 
 const Flight = model("Flight", flightSchema);
 
